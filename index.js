42a1bc5566498d91ffca3d0a4f85bb9f137dc619
hgpark <digitalworks_hgpark@amorepacific.com>
2023-05-02 13:43:16 +0900

    graphQl, resolvers(Query, Mutation)

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import { ApolloServer, gql } from "apollo-server";
 
+let tweets = [
+  {
+    id: "1",
+    text: "text",
+  },
+  {
+    id: "2",
+    text: "text2",
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
@@ -15,11 +26,38 @@ const typeDefs = gql`
     tweet(id: ID!): Tweet
   }
   type Mutation {
-    postTweet(text:String!, userId:ID!): Tweet!
-    deleteTweet(id:ID!): Boolean!
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((e) => e.id === id);
+    },
+  },
+  Mutation: {
+    postTweet(root, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(root, { id }) {
+      const tweet = tweets.find((e) => e.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((e) => e.id !== id);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => console.log(url));
